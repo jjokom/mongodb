df7dbc72b88985586c49ea341812b36fde0fc885
jjokom <john_jokom@yahoo.com>
2016-03-23 20:57:39 -0700

    Initial commit

---
+++ mongodb.py
@@ -0,0 +1,35 @@
+from flask import Flask
+
+from mongodb_demo import MongoDBTest
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+
+@app.route('/create/<username>')
+def create_user_profile(username):
+    # show the user profile for that user
+    mongodb_test.create(username)
+    return 'User %s' % username
+
+@app.route('/read')
+def show_all_user_profile():
+    # show the user profile for that user
+
+    return mongodb_test.read_all_user()
+
+
+
+if __name__ == '__main__':
+    mongodb_test = MongoDBTest()
+    app.run()
---
+++ mongodb_demo.py
@@ -0,0 +1,77 @@
+import sys
+import pymongo
+from datetime import datetime
+from pymongo import MongoClient
+from pymongo import ReturnDocument
+
+
+class MongoDBTest:
+    def __init__(self):
+        self.host = '172.16.0.11'
+        self.port = 30170
+        self.database_name = "test"
+        self.collection_name = "collection1"
+        
+        self.client = MongoClient(self.host, self.port)
+        self.database = self.client[self.database_name]
+        self.collection = self.database[self.collection_name]
+        
+    def create(self, name):
+        data = {"name": name,
+            "created_date": datetime.utcnow()}
+        print self.collection.insert_one(data)
+        
+    def read(self, name):
+        print self.collection.find_one({"name": name})
+        
+    def update(self, name, new_name):
+        print self.collection.find_one_and_update({"name": name}, {'$set': {'name': new_name}}, return_document=ReturnDocument.AFTER)
+        
+    def delete(self, name):
+        print self.collection.delete_one({"name": name})
+        
+    def read_all(self):
+        for data in self.collection.find():
+            print data
+
+    def read_all_user(self, ulist=None):
+  
+
+       ulist=""
+       for data in self.collection.find():
+            ulist = ulist+data['name'] + " "
+  
+       return ulist
+
+if len(sys.argv) < 2:
+    print "Usage: python mongodb_demo.py [create (name)|read (name)|update (name) (new_name)|delete (name)|read_all]"
+else:
+    mongodb_test = MongoDBTest()
+    action = sys.argv[1]
+    if action == "create":
+        if len(sys.argv) == 3:
+            mongodb_test.create(sys.argv[2])
+        else:
+            print "Usage: python mongodb_demo.py create (name)"
+    elif action == "read":
+        if len(sys.argv) == 3:
+            mongodb_test.read(sys.argv[2])
+        else:
+            print "Usage: python mongodb_demo.py read (name)"
+    elif action == "update":
+        if len(sys.argv) == 4:
+            mongodb_test.update(sys.argv[2], sys.argv[3])
+        else:
+            print "Usage: python mongodb_demo.py update (name) (new_name)"
+    elif action == "delete":
+        if len(sys.argv) == 3:
+            mongodb_test.delete(sys.argv[2])
+        else:
+            print "Usage: python mongodb_demo.py delete (name)"
+    elif action == "read_all":
+        if len(sys.argv) == 2:
+            mongodb_test.read_all()
+        else:
+            print "Usage: python mongodb_demo.py read_all"
+    else:
+        print "Usage: python mongodb_demo.py [create (name)|read (name)|update (name) (new_name)|delete (name)|read_all]"
